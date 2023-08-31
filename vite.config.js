1c22468bfadf2f5d28f65aab52660e52284cbd7f
owlsua <allatsnow@gmail.com>
2023-08-31 13:04:13 +0200

    chore: update app version fix version in the package-lock fix eslint, add eslint instruction to docs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
+      // eslint-disable-next-line no-undef
       entry: resolve(__dirname, 'lib/main.js'),
       name: 'EntropyGenerator',
       fileName: 'entropy-generator'
